1d2abf5ede6bb51260102d064ca87e3021f87364
Michael Nascimento <michael.nsc@outlook.com>
2023-01-16 21:20:30 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById('botao');
+//botao.onclick = aoClicar
+botao.addEventListener('click', aoClicar)
+
+function aoClicar() {
+    alert("Formulário salvo com sucesso!");
+}
